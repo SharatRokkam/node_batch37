8dfc840069853d9be1dba1b9a4e6b69ee3b901a3
SharatRokkam <rokkamsharath1@gmail.com>
2024-10-29 13:29:18 +0530

    multer and streams

---
+++ 29_Oct/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  return res.render("homepage");
+});
+
+app.post("/upload", upload.single("profileImage"), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+
+  return res.redirect("/");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
